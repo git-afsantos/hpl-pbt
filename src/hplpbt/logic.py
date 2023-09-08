eeb7385b6c7b3c1de7fe20e8073dd675ce84a910
André Santos <git-afsantos@users.noreply.github.com>
2023-09-08 14:46:12 +0100

    Update logic.py

--- src/hplpbt/logic.py
+++ src/hplpbt/logic.py
@@ -28,15 +28,15 @@ def split_assumptions(
     for hpl_property in hpl_properties:
         assert isinstance(hpl_property, HplProperty)
         if hpl_property.pattern.is_absence:
-            a, b = _split_absence(hpl_property, input_channels)
+            a, b = _split_safety(hpl_property, input_channels)
         elif hpl_property.pattern.is_existence:
-            a, b = _split_existence(hpl_property, input_channels)
+            a, b = _split_liveness(hpl_property, input_channels)
         elif hpl_property.pattern.is_requirement:
-            a, b = _split_requirement(hpl_property, input_channels)
+            a, b = _split_safety(hpl_property, input_channels)
         elif hpl_property.pattern.is_response:
-            a, b = _split_response(hpl_property, input_channels)
+            a, b = _split_liveness(hpl_property, input_channels)
         elif hpl_property.pattern.is_prevention:
-            a, b = _split_prevention(hpl_property, input_channels)
+            a, b = _split_safety(hpl_property, input_channels)
         else:
             raise TypeError(f'unknown HPL property type: {hpl_property!r}')
         assumptions.extend(a)
@@ -49,7 +49,7 @@ def split_assumptions(
 ###############################################################################
 
 
-def _split_absence(
+def _split_safety(
     hpl_property: HplProperty,
     input_channels: Container[str],
 ) -> Tuple[List[HplProperty], List[HplProperty]]:
@@ -71,33 +71,12 @@ def _split_absence(
     return assumptions, behaviour
 
 
-def _split_existence(
+def _split_liveness(
     hpl_property: HplProperty,
     input_channels: Container[str],
 ) -> Tuple[List[HplProperty], List[HplProperty]]:
-    # optimization: avoid creating new objects if the event is simple
-    return [], [hpl_property]
-
-
-def _split_requirement(
-    hpl_property: HplProperty,
-    input_channels: Container[str],
-) -> Tuple[List[HplProperty], List[HplProperty]]:
-    # optimization: avoid creating new objects if the event is simple
-    return [], [hpl_property]
-
-
-def _split_response(
-    hpl_property: HplProperty,
-    input_channels: Container[str],
-) -> Tuple[List[HplProperty], List[HplProperty]]:
-    # optimization: avoid creating new objects if the event is simple
-    return [], [hpl_property]
-
-
-def _split_prevention(
-    hpl_property: HplProperty,
-    input_channels: Container[str],
-) -> Tuple[List[HplProperty], List[HplProperty]]:
-    # optimization: avoid creating new objects if the event is simple
+    # FIXME: this logic is not quite right
+    for b in hpl_property.pattern.behaviour.simple_events():
+        if b.name in input_channels:
+            return [hpl_property], []
     return [], [hpl_property]
