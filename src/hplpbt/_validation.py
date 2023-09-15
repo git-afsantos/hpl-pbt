68c52c7d1e6dfc11e58d7f0485d036f959a5be4e
André Santos <git-afsantos@users.noreply.github.com>
2023-09-15 11:00:37 +0100

    Update _validation.py

--- src/hplpbt/_validation.py
+++ src/hplpbt/_validation.py
@@ -36,24 +36,52 @@ def _validate_type_def(name: str, type_def: Mapping[str, Any]):
     module: Optional[str] = type_def.get('import')
     if module is not None and not isinstance(module, str):
         raise TypeError(f"expected str 'import' value for {name}, found {module!r}")
-    params: Iterable[Any] = type_def['params']
-    if not isinstance(params, Iterable):
-        raise TypeError(f"expected Iterable 'params' value for {name}, found {params!r}")
-    for item in params:
-        if isinstance(item, str):
-            continue
-        if isinstance(item, Mapping):
-            _validate_param_constraints(name, item)
-            continue
-        raise TypeError(f"expected str or Mapping 'params' entry for {name}, found {item!r}")
+    try:
+        _validate_type_pos_args(type_def.get('args', ()))
+        _validate_type_kwargs(type_def.get('kwargs', {}))
+    except TypeError as err:
+        raise TypeError(f"in type definition '{name}': {err}") from err
+    except ValueError as err:
+        raise ValueError(f"in type definition '{name}': {err}") from err
 
 
-def _validate_param_constraints(name: str, entry: Mapping[str, Any]):
-    value = entry.get('type')
-    if value is None:
-        raise ValueError(f"parameter without a 'type' in {name}")
-    if not isinstance(value, str):
-        raise TypeError(f"expected str 'type' value for parameter of {name}, found {value!r}")
+def _validate_type_pos_args(args: Iterable[Union[str, Mapping[str, Any]]]):
+    if not isinstance(args, Iterable):
+        raise TypeError(f"expected Iterable 'args' value, found {args!r}")
+    for param_type in args:
+        try:
+            _validate_param_type(param_type)
+        except TypeError as err:
+            raise TypeError(f"in 'args' entry: {err}") from err
+        except ValueError as err:
+            raise ValueError(f"in 'args' entry: {err}") from err
+
+
+def _validate_type_kwargs(kwargs: Mapping[str, Union[str, Mapping[str, Any]]]):
+    if not isinstance(kwargs, Mapping):
+        raise TypeError(f"expected Mapping 'kwargs' value, found {kwargs!r}")
+    for name, param_type in kwargs.items():
+        if not isinstance(name, str):
+            raise TypeError(f"expected str 'kwargs' key, found {name!r}")
+        try:
+            _validate_param_type(param_type)
+        except TypeError as err:
+            raise TypeError(f"in 'kwargs' value for '{name}': {err}") from err
+        except ValueError as err:
+            raise ValueError(f"in 'kwargs' value for '{name}': {err}") from err
+
+
+def _validate_param_type(param_type: Union[str, Mapping[str, Any]]):
+    if isinstance(param_type, str):
+        return
+    if isinstance(param_type, Mapping):
+        type_name = param_type.get('type')
+        if type_name is None:
+            raise ValueError(f"missing key 'type'")
+        if not isinstance(type_name, str):
+            raise TypeError(f"expected str for value of parameter 'type', found {type_name!r}")
+        return
+    raise TypeError(f"expected str|Mapping parameter type, found {param_type!r}")
 
 
 ###############################################################################
@@ -64,7 +92,7 @@ def _validate_param_constraints(name: str, entry: Mapping[str, Any]):
 def param_from_data(data: Union[str, Mapping[str, Any]]) -> ParameterDefinition:
     if isinstance(data, str):
         data = {'type': data}
-    name: str = data.get('name', '')
+    # name: str = data.get('name', '')
     type_string: str = check_type(data['type'], str)
-    return ParameterDefinition(type_string, name=name)
-
+    constraints = {key: value for key, value in data.items() if key != 'type'}
+    return ParameterDefinition(type_string, constraints=constraints)
