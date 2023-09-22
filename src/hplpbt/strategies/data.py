c89593eb891cc3aabdd3d7b3e0cc409ea0e3007a
André Santos <git-afsantos@users.noreply.github.com>
2023-09-22 16:12:13 +0100

    WIP

--- src/hplpbt/strategies/data.py
+++ src/hplpbt/strategies/data.py
@@ -13,12 +13,14 @@ from attrs import define, field
 from hpl.ast import HplExpression
 from hpl.types import TypeToken
 
+from hplpbt.strategies.ast import ConstantValue, DataStrategy, Expression, RandomSample
+
 ################################################################################
 # Internal Structures: Basic (Data) Field Generator
 ################################################################################
 
 
-class FieldGeneratorState(Enum):
+class DataGeneratorState(Enum):
     PENDING = auto()  # waiting for the parent to be initialized
     READY = auto()  # the parent is available, this can be initialized
     INITIALIZED = auto()  # the field has a value, but there is more to do
@@ -26,25 +28,37 @@ class FieldGeneratorState(Enum):
 
 
 @define
-class FieldGenerator:
+class DataGenerator:
     """
-    A FieldGenerator is composed of multiple statements (initialization,
+    A DataGenerator is composed of multiple statements (initialization,
     assumptions, ...) and each statement has its own dependencies (references
     to other local/external fields) so that they can be sorted individually.
     This maximizes flexibility, and results in code that is closer to what a
     human would write, knowing in advance what each statement needs.
-    Internally, the FieldGenerator can be seen as a sort of state machine.
+    Internally, the DataGenerator can be seen as a sort of state machine.
     When generating code, it changes its state as the requirements of each
     statement are satisfied and the statements are processed.
     """
 
     expression: HplExpression
     type_token: TypeToken
-    parent: Optional['FieldGenerator']
-    strategy: Any
-    assumptions: List[Any] = field(factory=list)
+    parent: Optional['DataGenerator']
+    strategy: DataStrategy
+    assumptions: List[HplExpression] = field(factory=list)
     is_ranged: bool = False
     reference_count: int = field(default=0, init=False, eq=False)
     _ready_ref: Any = None
     _init_ref: Any = None
     _loop_context: Optional[Any] = None
+
+    def eq(self, value: Expression):
+        if not self.strategy.is_constant:
+            self.strategy = ConstantValue(value)
+
+    def neq(self, value: Expression):
+        if self.strategy.is_sample:
+            assert isinstance(self.strategy, RandomSample)
+            self.strategy = self.strategy.remove(value)
+            if len(self.strategy.elements) <= 0:
+                raise ContradictionError('{self.expression} != {value}')
+        self.assumptions.append(_Assumption(self._init_ref, value, "!="))
