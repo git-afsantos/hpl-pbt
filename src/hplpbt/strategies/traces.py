83d8cfe697b2ccfdcfcb599752bb476b2a968217
André Santos <git-afsantos@users.noreply.github.com>
2023-11-09 16:15:41 +0000

    Update traces.py

--- src/hplpbt/strategies/traces.py
+++ src/hplpbt/strategies/traces.py
@@ -7,9 +7,18 @@
 
 from typing import Final, Iterable, List, Mapping, Optional, Set, Union
 
-from attrs import field, frozen
+from attrs import evolve, field, frozen
 from attrs.validators import deep_iterable, deep_mapping, instance_of
-from hpl.ast import HplEvent, HplProperty, HplSimpleEvent, HplSpecification
+from hpl.ast import (
+    HplEvent,
+    HplExpression,
+    HplPattern,
+    HplPredicate,
+    HplProperty,
+    HplScope,
+    HplSimpleEvent,
+    HplSpecification,
+)
 from hplpbt.strategies.messages import MessageStrategy, MessageStrategyBuilder
 from typeguard import typechecked
 
@@ -92,7 +101,10 @@ class TraceStrategyBuilder:
     )
     assumptions: Iterable[HplProperty] = field(
         factory=list,
-        validator=deep_iterable(instance_of(HplProperty), iterable_validator=instance_of(Iterable))
+        validator=deep_iterable(
+            instance_of(HplProperty),
+            iterable_validator=instance_of(Iterable),
+        ),
     )
 
     @input_channels.validator
@@ -118,50 +130,113 @@ class TraceStrategyBuilder:
         # first segment: activator
         event = hpl_property.scope.activator
         if event is not None:
-            seg = self.publish_segment(event)
-            segments.append(seg)
+            segments.append(self.publish_segment(event))
 
         # main segments: pattern-based events
         if hpl_property.pattern.is_absence:
-            pass  # spam segment
+            segments.append(self.spam_segment())
         elif hpl_property.pattern.is_existence:
-            pass  # spam segment
+            segments.append(self.spam_segment())
         elif hpl_property.pattern.is_requirement:
             event = hpl_property.pattern.trigger
             assert event is not None
-            # spam avoid trigger segment
+            # FIXME if timed pattern, do not avoid trigger all the time
+            segments.append(self.spam_segment_avoiding(event))
         elif hpl_property.pattern.is_response:
             event = hpl_property.pattern.trigger
             assert event is not None
-            # trigger segment
-            # spam segment
+            # FIXME include pattern duration
+            segments.append(self.publish_segment(event))
+            segments.append(self.spam_segment())
         elif hpl_property.pattern.is_prevention:
             event = hpl_property.pattern.trigger
             assert event is not None
-            # trigger segment
-            # spam segment
+            # FIXME include pattern duration
+            segments.append(self.publish_segment(event))
+            segments.append(self.spam_segment())
         else:
             raise TypeError(f'unknown HPL property type: {hpl_property!r}')
 
         # last segment: terminator
         event = hpl_property.scope.terminator
         if event is not None:
-            seg = self.publish_segment(event)
-            segments.append(seg)
+            segments.append(self.publish_segment(event))
 
         return TraceStrategy(segments=segments)
 
     def publish_segment(self, event: HplEvent) -> TraceSegment:
-        builder = MessageStrategyBuilder(
-            self.input_channels,
-            self.type_defs,
-            assumptions=self.assumptions
-        )
+        segment: TraceSegment = self.spam_segment_avoiding(event)
+        # FIXME apply general trace assumptions
+        builder = MessageStrategyBuilder(self.input_channels, self.type_defs)
         mandatory: Set[MessageStrategy] = set()
-        helpers: Set[MessageStrategy] = set()
+        helpers: Set[MessageStrategy] = set(segment.helpers)
         for ev in event.simple_events():
             assert isinstance(ev, HplSimpleEvent)
             strategy, dependencies = builder.build_pack_from_simple_event(ev)
             mandatory.add(strategy)
             helpers.update(dependencies)
-        return TraceSegment(0, INF, mandatory=mandatory, helpers=helpers)
+        return evolve(segment, mandatory=mandatory, helpers=helpers)
+
+    def spam_segment_avoiding(self, event: HplEvent) -> TraceSegment:
+        # build assumptions to avoid all possible triggers
+        anti_triggers: Mapping[str, HplPredicate] = {}
+        for ev in event.simple_events():
+            assert isinstance(ev, HplSimpleEvent)
+            # discard unknown input channels
+            type_name: Optional[str] = self.input_channels.get(ev.name)
+            if type_name is None:
+                continue
+            # store predicates per message type
+            phi: Optional[HplPredicate] = anti_triggers.get(type_name)
+            if phi is None:
+                phi = ev.predicate.negate()
+            else:
+                phi = phi.join(ev.predicate.negate())
+            anti_triggers[type_name] = phi
+        # FIXME must do something with general trace assumptions
+        assumptions = list(self.assumptions)
+        for name, phi in anti_triggers.items():
+            scope = HplScope.globally()
+            behaviour = HplSimpleEvent.publish(name, phi)
+            pattern = HplPattern.absence(behaviour)
+            assumptions.append(HplProperty(scope, pattern))
+        # ------------------------------------------------------
+        return self.spam_segment(conditions=anti_triggers)
+
+    def spam_segment(self,
+        conditions: Optional[Mapping[str, HplExpression]] = None,
+    ) -> TraceSegment:
+        # build spam messages that avoid trigger conditions
+        new_type_defs = _apply_extra_type_conditions(self.type_defs, conditions)
+        builder = MessageStrategyBuilder(self.input_channels, new_type_defs)
+        spam: Set[MessageStrategy] = set()
+        helpers: Set[MessageStrategy] = set()
+        for type_name in self.input_channels.values():
+            strategy, dependencies = builder.build_pack_for_type_name(type_name)
+            spam.add(strategy)
+            helpers.update(dependencies)
+        return TraceSegment(0, INF, spam=spam, helpers=helpers)
+
+
+################################################################################
+# Helper Functions
+################################################################################
+
+
+def _apply_extra_type_conditions(
+    type_defs: Mapping[str, MessageType],
+    conditions: Optional[Mapping[str, HplPredicate]],
+) -> Mapping[str, MessageType]:
+    if not conditions:
+        return type_defs
+    new_type_defs = {}
+    for name, type_def in type_defs.items():
+        phi = conditions.get(name)
+        if phi is None:
+            new_type_defs[name] = type_def
+        else:
+            # TODO maybe evolve a new name for the type def
+            # to use as strategy function name and avoid collisions
+            phi = type_def.precondition.join(phi)
+            new_type_defs[name] = evolve(type_def, precondition=phi)
+    return new_type_defs
